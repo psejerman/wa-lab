f86e8f5401368f01d3d3a896e1cd02de7fa4ac51
FynnHa <75836763+FynnHa@users.noreply.github.com>
2021-01-07 11:23:54 +0100

    php

--- uebung2/cgi-bin/post.php
+++ uebung2/cgi-bin/post.php
@@ -1,5 +1,11 @@
 <?php
-  echo "<pre>";
-  print_r($_POST);
-  echo "</pre>";
+    echo "Deine Email ist: <b>";
+    echo $_POST['email'];
+    echo "</b></br>Dein Nachname ist: <b>";
+    echo $_POST['name'];
+    echo "</b></br>Dein Passwort ist: <b>*****</b></br>Dein Datum ist: <b>";
+    echo $_POST['date'];
+    echo "</b></br>Dein Gender ist: <b>";
+    echo $_POST['gender'];
+    echo "</b></br></br><a href=\"javascript:history.go(-1)\">GO BACK</a>";
 ?>
