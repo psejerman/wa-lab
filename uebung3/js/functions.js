39e016dff6118f7419d60f5dc40f44e9285edd9e
Peter Sejerman <p.sejerman@gmail.com>
2021-01-31 19:02:15 +0100

    bla

--- uebung3/js/functions.js
+++ uebung3/js/functions.js
@@ -1,11 +1,18 @@
-function ajax(param) {
+function load(param) {
   // Instanz von XMLHttpRequest()
   var xhttp = new XMLHttpRequest();
+  //Verhalten bei Eingang von Antwort
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById("index-layout").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", param + '.html', true);
+  xhttp.open("POST", param + '.html', true);
   xhttp.send();
 }
+
+function loadContent() {
+  if (this.readyState == 4 && this.status == 200) {
+   document.getElementById("index-layout").innerHTML = this.responseText;
+  }
+};
